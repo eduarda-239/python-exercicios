45d5a5b906edb7181b157968425c645f05492dc1
Maria Eduarda <105071413+eduarda-239@users.noreply.github.com>
2026-05-29 21:35:16 -0300

    Create exercicio01.py
    Adicionando exercício de lista de clientes.

---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+# Programa que processa uma lista de 5 nomes de clientes para gerar relatórios mensais. Percorra a lista de nome e exiba cada cliente:
+# Ex: clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
+
+clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
+
+for cliente in clientes:
+  print(cliente)
