8f5528e7f0a39c6a2ea801b10fe4f18419e404ac
Maria Eduarda <105071413+eduarda-239@users.noreply.github.com>
2026-05-29 22:09:49 -0300

    Create soma.py
    Variável soma dentro de uma lista.

---
+++ soma.py
@@ -0,0 +1,8 @@
+# Programa que exibe o valor da soma dos números de uma lista:
+
+numeros = [10, 20, 30, 40, 50]  # Aqui criamos a lista. 
+soma = 0     # Variável soma. 
+for numero in numeros:    # Para cada número dentro da lista "numeros", faça algo. Primeiro é 10, segundo é 20, terceiro é 30...
+    soma += numero     # Significa "soma = soma + numero".
+print(f"A soma total das receitas é: {soma}")      # Aparece a frase com a variável.
+
