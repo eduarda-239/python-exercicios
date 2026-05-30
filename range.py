6160561be1931608c515579cbbe0052419875b7e
Maria Eduarda <105071413+eduarda-239@users.noreply.github.com>
2026-05-29 22:01:03 -0300

    Create range.py
    Replicar uma frase um determinado número de vezes.

---
+++ range.py
@@ -0,0 +1,4 @@
+# Escreva um programa que exiba a mensagem: "Bem-vindo ao Buscante!" o número exato de vezes que ele precisa.
+
+for i in range(5):          # Cria uma sequência de número (0,1,2,3,4)
+    print("Bem-vindo ao Buscante!")      # Esse comando será executado 5 vezes.
